d83a3c5ea2861bd1042655191ba676d7032eb424
stanggren <astanggren@gmail.com>
2020-11-04 14:43:57 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// if function does not have 
+window.onload = main;
+
+function getAllPlayers() {
+    return players
+}
+/**
+ * Init program
+ */
+function main() {
+    startSlideShowAnimation();
+}
+
+function startSlideShowAnimation(){
+    setInterval(changeActiveImage, 4000)
+    changeActiveImage();
+}
+
+function changeActiveImage() {
+    const images = document.querySelectorAll('.slideshow-container > img');
+    // const currentImage = images.find(img => img ) 
+
+    for (let i = 0; i < images.length; i++){
+        if (images[i].classList.contains('show')) {
+            images[i].classList.remove('show');
+
+
+            // go to first image
+            // modulus 4 (fyra bilder)
+            // modulus 3 (tre bilder)
+            let nextIndex = (i + 1) % images.length;
+            // if (nextIndex >= images.length) {
+                
+            //     nextIndex = 0;
+            // }
+
+            images[nextIndex].classList.add('show');
+            break;
+        }
+    }
+}
+
---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+const projects = [
+    dashboard = {
+        title: 'Dashboard',
+        text: 'A dashboard which consumes data from a document database and displays it through charts and tables. The dashboard also includes a date picker where the user can set a desired times pan for the data to be displayed in.',
+        tools: ['C# (ASP.NET', 'Vue.js (Vuex)', 'HTML', 'CSS', 'Elasticsearch', 'Highcharts', 'GIT'],
+        link: 'https://github.com/stanggren',
+        imgSrc : '/images/thumbnails/dashboard-tn.png',
+        modalImgSrc: '/images/modals/dashboard.jpg'
+    },
+    recipeSearch = {
+        title: 'Recipe Search',
+        text: 'Using an API from Edamam the user can search recipes and tick off ingredients from each recipe.',
+        tools: ['React.js, HTML, CSS'],
+        link: 'https://github.com/stanggren/recipe-api',
+        imgSrc : '/images/thumbnails/recipe-search-tn.png',
+        modalImgSrc: '/images/modals/recipe-search.png'
+    },
+    diceGame = {
+        title: 'Dice Game',
+        text: 'DOM game. The game has two players. In each turn a player roll a dice as many times as they wish. Each result gets added to their round score. First player to reach winning score wins the game.',
+        tools: ['JavaScript, HTML, CSS'],
+        link: 'https://github.com/stanggren/Dice-Game',
+        imgSrc : '/images/dice-game.png',
+        modalImgSrc: '/images/modals/dice-game.png'
+    },
+    dashboard = {
+        title: 'Planet Jumper',
+        text: 'A dashboard which consumes data from a document database and displays it through charts and tables. The dashboard also includes a date picker where the user can set a desired times pan for the data to be displayed in.',
+        tools: ['C# (ASP.NET', 'Vue.js (Vuex)', 'HTML', 'CSS', 'Elasticsearch', 'Highcharts', 'GIT'],
+        link: 'https://github.com/stanggren/planet-jumper',
+        imgSrc : '/images/planet-jumper.png',
+        modalImgSrc: '/images/modals/planet-jumper.png'
+    },
+
+]
