7377cbcae69788d060a5d1daeb6ed07b560d6f8b
stanggren <astanggren@gmail.com>
2020-11-09 21:53:32 +0100

    modal JSDoc

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,16 +1,32 @@
-// Get the modal
+/**
+ * @type {HTMLElement} modal
+ */
 const modal = document.getElementById("myModal");
+/**
+ * @type {HTMLElement} modalTitle
+ */
 const modalTitle = document.getElementById("modal-title");
+/**
+ * @type {HTMLElement} modalImg
+ */
 const modalImg = document.getElementById("modal-img")
 
 
 // When the user clicks anywhere outside of the modal, close it
+/**
+ * User clicks anywhere outside of the modal and closes the modal.
+ * @param {MouseEvent} event 
+ */
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
+/**
+ * User clicks image to display modal, modal image and modal title.
+ * @param {String} imgTitle 
+ */
 function showModal(imgTitle){
   modal.style.display = "block";
   modalTitle.textContent = imgTitle;
@@ -18,6 +34,10 @@ function showModal(imgTitle){
   modalImg.src = setImageSrc(imgTitle);
 }
 
+/**
+ * Conditional rendering of modal image depending on what image user clicked on.
+ * @param {String} title sets parameter depending on what image clicked.
+ */
 function setImageSrc(title){
     console.log(projects[0].modalImgSrc);
     switch(title) {
