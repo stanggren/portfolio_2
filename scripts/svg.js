47968788f3cc23843c68cabdae5d90e7be3cf354
stanggren <astanggren@gmail.com>
2020-11-09 22:44:09 +0100

    svg JSDoc

--- scripts/svg.js
+++ scripts/svg.js
@@ -1,8 +1,13 @@
+/**
+ * Displays SVG and runs animation on scroll event.
+ */
 function displaySVG() {
     const wrapper = document.getElementById("svg-container");
     const parent = document.getElementById("Layer_1");
     wrapper.style.opacity = "1";
     wrapper.style.animation = "shadowOpacity 7s"
+    
+    // sets path class. Runs animation on each individual SVG path
     for (let i = 1; i < parent.childNodes.length; i+=2 ){
         parent.childNodes[i].classList.add("path");
     }
