af8dd43d0e511d39c833aca3248226cdf59dba78
stanggren <astanggren@gmail.com>
2020-11-10 15:01:19 +0100

    update projects img src

--- scripts/projects.js
+++ scripts/projects.js
@@ -6,32 +6,32 @@ const projects = [
         title: 'Dashboard',
         text: 'A dashboard which consumes data from a document database and displays it through charts and tables. The dashboard also includes a date picker where the user can set a desired times pan for the data to be displayed in.',
         link: 'https://github.com/stanggren',
-        imgSrc : './images/thumbnails/dashboard-tn.png',
-        modalImgSrc: './images/modals/dashboard.jpg',
+        imgSrc : '../images/thumbnails/dashboard-tn.png',
+        modalImgSrc: '../images/modals/dashboard.jpg',
         alt: 'Dashboard'
     },
     recipeSearch = {
         title: 'Recipe Search',
         text: 'Using an API from Edamam the user can search recipes and tick off ingredients from each recipe.',
         link: 'https://github.com/stanggren/recipe-api',
-        imgSrc : './images/thumbnails/recipe-search-tn.png',
-        modalImgSrc: './images/modals/recipe-search.png',
+        imgSrc : '../images/thumbnails/recipe-search-tn.png',
+        modalImgSrc: '../images/modals/recipe-search.png',
         alt: 'Recipe Search'
     },
     diceGame = {
         title: 'Dice Game',
         text: 'DOM game. The game has two players. In each turn a player roll a dice as many times as they wish. Each result gets added to their round score. First player to reach winning score wins the game.',
         link: 'https://github.com/stanggren/Dice-Game',
-        imgSrc : './images/modals/dice-game.png',
-        modalImgSrc: './images/modals/dice-game.png',
+        imgSrc : '../images/modals/dice-game.png',
+        modalImgSrc: '../images/modals/dice-game.png',
         alt: 'Dice Game'
     },
     planetJumper = {
         title: 'Planet Jumper',
         text: 'A dashboard which consumes data from a document database and displays it through charts and tables. The dashboard also includes a date picker where the user can set a desired times pan for the data to be displayed in.',
         link: 'https://github.com/stanggren/planet-jumper',
-        imgSrc : './images/modals/planet-jumper.png',
-        modalImgSrc: './images/modals/planet-jumper.png',
+        imgSrc : '../images/modals/planet-jumper.png',
+        modalImgSrc: '../images/modals/planet-jumper.png',
         alt: 'Planet Jumper'
     },
 ]
